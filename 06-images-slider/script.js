7ad51f6624f7103946c5de0e8132c3348a33c7b8
Qianxill <hsb264@alumni.ku.dk>
2024-06-17 06:36:31 +0200

    06-images-slider

---
+++ 06-images-slider/script.js
@@ -0,0 +1,14 @@
+const imgs = document.querySelectorAll(".panel");
+
+imgs.forEach((img) => {
+  img.addEventListener('click', () => {
+    removeActiveClasses();
+    img.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  imgs.forEach((img) => {
+    img.classList.remove("active");
+  });
+}
